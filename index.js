be34cc8401e73048d6b78c818a3bc2477a4897b9
wandera7 <julius.wandera@student.moringaschool.com>
2022-06-08 09:00:27 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee={
+    name:'Sam',
+    address:'11 Broadway'
+}
+
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    const newEmployee={...obj};
+    newEmployee[key]=value;
+    return newEmployee;
+}
+function  destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    obj[key]=value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj,key){
+   const newEmployee={...obj};
+   delete newEmployee[key];
+   return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete obj[key];
+    return obj;
+}
